8ec562867c9d45b8043b0b88d222ad97c68a7f7f
Chad McKinney <cfmckinney@gmail.com>
2019-06-22 10:50:04 -0600

    Dialog Activator
    Dialog Activator

--- 2D-Escape-Roomv2/Assets/Scripts/PlayerController.cs
+++ 2D-Escape-Roomv2/Assets/Scripts/PlayerController.cs
@@ -8,25 +8,35 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D theRB;
     public float moveSpeed;
 
+    public bool canMove = true;
+
+    public static PlayerController instance;
+
     public Animator myAnim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        instance = this;
     }
 
     // Update is called once per frame
     void Update()
     {
-      theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed; 
+        if(canMove){
+      theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+        } else {
+            theRB.velocity=Vector2.zero;
+        }
 
       myAnim.SetFloat("moveX", theRB.velocity.x);
       myAnim.SetFloat("moveY", theRB.velocity.y);
 
       if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
       {
+          if(canMove){
           myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
           myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+          }
       }
     }
 }
