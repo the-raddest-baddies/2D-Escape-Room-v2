4133a18ee8333c550387bb6e20991895df593f56
Mark Romero <markromero@Marks-MacBook-Pro.local>
2019-06-25 16:47:19 -0600

    fixed the menu opening and closing

---
+++ 2D-Escape-Roomv2/Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(pauseMenu.activeInHierarchy)
+            {
+                pauseMenu.SetActive(false);
+                //PlayerController.instance.canMove = true;
+            }
+            else 
+            {
+                pauseMenu.SetActive(true);
+                //PlayerController.instance.canMove = false;
+
+            }
+        }
+        
+    }
+}
