9ce9595d01532d10b0205b7b107827754c4a3d27
Chad McKinney <cfmckinney@gmail.com>
2019-06-26 23:00:18 -0600

    Added Interaction Script and UI
    Layers got all screwey. Also, I added Crafting Recipe, fishing pole asset, and note asset and text.

---
+++ 2D-Escape-Roomv2/Assets/Scripts/InteractionUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractionUI : MonoBehaviour
+{
+    public GameObject interactText;
+    private bool canInteract;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canInteract && !DialogManager.instance.dialogBox.activeInHierarchy){
+            interactText.SetActive(true);
+        }else{interactText.SetActive(false);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "Player") {
+            canInteract = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other){
+        if(other.tag == "Player") {
+            canInteract = false;
+        }
+    }
+    // public void ShowInteraction(string newAction)
+    // {
+    //     itemInteract = newAction;
+
+    //     interactText.text = itemInteract;
+    //     interactionUI.SetActive(true);
+    // }
+}
