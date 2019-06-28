2c67e6167340b85adf920df4805cab12474a85d8
Mark Romero <markromero@Marks-MacBook-Pro.local>
2019-06-27 19:05:17 -0600

    added animations for the gas

---
+++ 2D-Escape-Roomv2/Assets/Scripts/ActivateGas.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateGas : MonoBehaviour
+{
+  [SerializeField] private Animator myAnimationController;
+
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+      if(other.CompareTag("Player")){
+          myAnimationController.SetBool("GasOn", true);
+      }
+  }
+}
